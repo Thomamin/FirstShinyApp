9db60a36a35ade6c4986d44c270f215795392ad7
Thomamin <thomamin@hotmail.com>
2014-11-26 03:28:00 +0900

    restore deleted shinyapp

---
+++ server.R
@@ -0,0 +1,19 @@
+hpiData <- read.csv("./HPI.csv")
+
+shinyServer(
+  function(input, output) {
+    
+    output$happinessRank <- renderText({
+        paste("Happiness rank of your country is", 
+                      hpiData[hpiData$Country == input$userCountry,'HPIRank'])
+    })
+    output$lifeExpectancy <- renderText({
+        paste("Life expectancy of your country is ", 
+                      hpiData[hpiData$Country == input$userCountry, 'LifeExpectancy'])
+    })
+    output$remainingYears <- renderText({
+        paste("Remaining years for you: ", 
+              hpiData[hpiData$Country == input$userCountry, 'LifeExpectancy'] - input$userAge)
+    })
+  } 
+)
